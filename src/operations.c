964151c60c0c37cc983b686152033e79d0f3c42b
João Gomes <joaogomesdev32@gmail.com>
2026-06-04 12:26:30 +0100

    :sparkles: implement ss operation and improved with static function swap

--- src/operations.c
+++ src/operations.c
@@ -6,38 +6,46 @@
 /*   By: joaog <joaog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 11:35:05 by joaog             #+#    #+#             */
-/*   Updated: 2026/06/04 12:04:40 by joaog            ###   ########.fr       */
+/*   Updated: 2026/06/04 12:24:12 by joaog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void	op_sa(t_stacks *s)
+static void	swap(t_node **stack)
 {
 	int	first;
 	int	second;
 
+	if (!*stack || !(*stack)->next)
+		return ;
+	first = pop_top(stack);
+	second = pop_top(stack);
+	push_top(stack, first);
+	push_top(stack, second);
+}
+
+void	op_sa(t_stacks *s)
+{
 	if (!s)
 		return ;
-	first = pop_top(&s->a);
-	second = pop_top(&s->a);
-	push_top(&s->a, first);
-	push_top(&s->a, second);
+	swap(&s->a);
 	write(1, "sa\n", 3);
 }
 
 void	op_sb(t_stacks *s)
 {
-	int	first;
-	int	second;
-
 	if (!s)
 		return ;
-	first = pop_top(&s->b);
-	second = pop_top(&s->b);
-	push_top(&s->b, first);
-	push_top(&s->b, second);
+	swap(&s->b);
 	write(1, "sb\n", 3);
 }
 
-
+void	op_ss(t_stacks *s)
+{
+	if (!s)
+		return ;
+	swap(&s->a);
+	swap(&s->b);
+	write(1, "ss\n", 3);
+}
